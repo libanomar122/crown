8ec8190d2a8c81bed68219ed4bbf02db41329445
Liban Omar <Libanomar122@gmail.com>
2024-08-08 15:27:02 -0500

    Created components folder
    Created category-item and categories components. Currently, the category-item is only coded.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,13 @@
-const App = () => {
-	const categories = [
-		{
-			id: 1,
-			title: "Hats",
-		},
-		{
-			id: 2,
-			title: "Jackets",
-		},
-		{
-			id: 3,
-			title: "Sneakers",
-		},
-		{
-			id: 4,
-			title: "Womens",
-		},
-		{
-			id: 5,
-			title: "Mens",
-		},
-	];
+import "./components/categories.styles.scss";
+import CategoryItem from "./components/category-item/category-item.component";
+import categories from "./categories.json";
 
-	return (
-		<div className="categories-container">
-			{categories.map(category => {
-				return (
-					<div className="category-container" key={category.id}>
-						{/* <img src="" alt="" /> */}
-						<div className="category-body-container">
-							<h2>{category.title}</h2>
-							<p>Shop Now</p>
-						</div>
-					</div>
-				)
-			})}
-			
-		</div>
-	);
-};
+const App = () => (
+	<div className="categories-container">
+		{categories.map((category) => (
+			<CategoryItem key={category.id} category={category} />
+		))}
+	</div>
+);
 
 export default App;
